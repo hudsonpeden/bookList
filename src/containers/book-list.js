e16b812d792446a3b96c7d7ab094d4b3c2c2e8fc
Hudson Peden <hpeden@logicalsysinc.com>
2017-03-01 17:31:59 -0600

    finished application

--- src/containers/book-list.js
+++ src/containers/book-list.js
@@ -1,10 +1,18 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { selectBook } from '../actions/index';
+import { bindActionCreators } from 'redux';
 
-export default class BookList extends Component {
+// this is a container
+class BookList extends Component {
   renderList() {
     return this.props.books.map((book) => {
       return (
-        <li key={book.title} className="list-group-item">{book.title}</li>
+        <li
+          key={book.title}
+          onClick={() => this.props.selectBook(book)}
+          className="list-group-item">
+          {book.title}</li>
       );
     });
   }
@@ -17,3 +25,26 @@ export default class BookList extends Component {
     )
   }
 }
+
+
+//This function is the glue between React and Redux
+function mapStateToProps(state) {
+  // Whatever is returned from here will show up
+  // as props inside BookList.
+  return{
+    books: state.books
+  };
+}
+
+// Anything returned from this function will end up as props
+// on the Booklist container.
+function mapDispatchToProps(dispatch) {
+  // Whenever selectBook is called, the function should be
+  // passed to all of our reducers.
+  return bindActionCreators({ selectBook: selectBook }, dispatch);
+}
+
+// Promote BookList from a Component to a container - it needs
+// to know about this new dispatch method, selectBook. Make it available
+// as a prop.
+export default connect(mapStateToProps, mapDispatchToProps)(BookList);
